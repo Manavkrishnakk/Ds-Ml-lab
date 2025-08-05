6a380e93a4379c9935fbb1dced7e1dcb344bbf6d
MANAV <manavkrishnakk25@gmail.com>
2025-08-05 13:57:25 +0530

    Add files via upload

---
+++ piechart.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+import numpy as np
+prg=np.array(["java","python","javascript","c#","c++"])
+pop=np.array([22.2,17.6,8.8,7.7,6.7])
+color=["red","green","yellow","blue","black","cyan"]
+plt.pie(pop,labels=prg,shadow=True,colors=color)
+plt.title("Piechart")
+plt.show();
---
+++ tdsum.py
@@ -0,0 +1,7 @@
+import numpy as np
+arr1=np.array([1,2],[3,4])
+arr2=np.array([6,7],[9,7])
+print(arr1)
+print(arr2)
+result=np.add(arr1,arr2)
+print("sum of array:",result)
